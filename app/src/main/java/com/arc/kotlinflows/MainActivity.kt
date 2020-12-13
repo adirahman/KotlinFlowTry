d2eadbf1b7c0f284ec0f4703e65e7491bf92bc5e
Adi <rahmanadi9@gmail.com>
2020-12-13 15:04:05 +0700

    iniit

---
+++ app/src/main/java/com/arc/kotlinflows/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.arc.kotlinflows
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val flow = flow {
+            for (i in 1..10){
+                emit(i)
+                delay(1000L)
+            }// producer
+        }
+
+        GlobalScope.launch {
+            flow.buffer().filter{
+                it % 2 == 0
+            }.map{
+                it * it
+            }.collect {
+                println(it)
+                delay(2000L)
+            }// consumer
+        }
+    }
+
+
+}
